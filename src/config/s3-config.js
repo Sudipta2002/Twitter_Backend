12869c07ec7e3eea634a46f3aeea83b44c359cd4
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2023-07-07 10:09:51 +0530

    s3config added

---
+++ src/config/s3-config.js
@@ -0,0 +1,26 @@
+import multer from 'multer';
+import multers3 from 'multers3';
+import aws from 'aws-sdk';
+
+aws.config.update({
+    region: process.env.AWS_REGION,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS,
+    accessKeyId: process.env.AWS_ACCESS_KEY
+});
+
+const s3 = new aws.S3();
+
+const upload = multer({
+    storage: multers3({
+        s3: s3,
+        bucket: process.env.BUCKET_NAME,
+        acl: 'public-read',
+        metadata: function(req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function(req, file, cb) {
+            cb(null, Date.now().toString())
+        }
+    })
+});
+export { upload };
